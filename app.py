85ffcd847ec4ee19e3bb633b854322ef2597fd63
Gabriel Rocha Dias <gabigolbr36@gmail.com>
2025-04-20 22:33:10 -0300

    add files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import sys
 sys.dont_write_bytecode = True
 from flask import Flask, request
-from routes.cliente import login
+from routes.cliente import login, register
 
 app = Flask(__name__)
 
@@ -11,5 +11,9 @@ def login_route():
         return login()
 
 
+@app.route("/register", methods=["GET", "POST"])
+def register_route():
+        return register()
+
 if __name__ == "__main__":
-    app.run(debug=True, host="localhost", port="3000")    
+    app.run(debug=True, port="3000", host="0.0.0.0")    
--- routes/cliente.py
+++ routes/cliente.py
@@ -1,16 +1,47 @@
-from flask import jsonify, make_response, request, render_template
+from flask import jsonify, make_response, request, render_template, redirect, url_for
 import sys
 from db import get_connection
 
 sys.dont_write_bytecode = True
 
 def login():
-    print("qualquer coisa")
     if request.method == "GET":
         # deverá retornar o html crú
         return render_template("login.html")
     
     elif request.method == "POST":
-        return make_response(jsonify("Post method"))
-        cursor = get_connection()
+        dados = request.form.to_dict()
+        conexao = get_connection()
+        cursor = conexao.cursor(dictionary=True)
+        cursor.execute("SELECT nome, chave, saldo FROM usuarios WHERE email = %s AND senha = %s", (dados["email"], dados["password"]))
+        usuario = cursor.fetchall()
+        conexao.close()
+        cursor.close()
+        if not usuario:
+            return redirect(url_for("login_route"))
+        
+        return render_template("home.html", nome=usuario[0]["nome"], chave_pix=usuario[0]["chave"], saldo=usuario[0]["saldo"])
 
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+    
+    elif request.method == "POST":
+        dados = request.form.to_dict()
+        conexao = get_connection()
+        cursor = conexao.cursor()
+        cursor.execute("""
+    INSERT INTO usuarios (nome, senha, data_nascimento, email, chave, telefone, saldo)
+    VALUES (%s, %s, %s, %s, %s, %s, %s)
+""", (
+    dados["nome"],
+    dados["senha"],
+    dados["data-nascimento"],
+    dados["email"],
+    dados["chave-pix"],
+    dados["telefone"],
+    "0"
+))
+        conexao.commit()
+        
+        return render_template("login.html")
